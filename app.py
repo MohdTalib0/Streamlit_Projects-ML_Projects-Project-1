e177874c6db9317590836618fd2f045a5a313931
MohdTalib0 <74108923+MohdTalib0@users.noreply.github.com>
2023-02-27 10:13:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.set_page_config(page_icon=":cry:", page_title="Dashboard")
+st.header("Welcome")
+#st.sidebar.success("EntryPoint")
+st.subheader("Customer Churn Prediction in Telecom Industry")
