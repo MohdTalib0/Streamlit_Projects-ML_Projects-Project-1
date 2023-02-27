d0bae9b2599689af303d2607038fbf58d6f297f1
MohdTalib0 <74108923+MohdTalib0@users.noreply.github.com>
2023-02-27 10:12:01 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-#import module
-import streamlit as st
- 
-#Title
-st.title("Hello GeeksForGeeks !!!")
- 
-#pip freeze > requirements.txt 
-#pip list to check all installed packages
-#python -m venv st-env to create virtual envoirment
